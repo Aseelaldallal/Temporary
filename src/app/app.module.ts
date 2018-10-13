1daa86904b343d1a7c321d53a5eea757a30e72f6
aseelaldallal <aseelaldallal@gmail.com>
2018-10-13 18:07:45 -0400

    backbone structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { QuickStartComponent } from './tabs/quick-start/quick-start.component';
+import { MoreComponent } from './tabs/more/more.component';
+import { ChatComponent } from './tabs/chat/chat.component';
+import { HelpComponent } from './tabs/help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    PermissionsComponent,
+    QuickStartComponent,
+    MoreComponent,
+    ChatComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule
